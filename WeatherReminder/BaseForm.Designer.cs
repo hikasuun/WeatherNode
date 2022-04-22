5dff2afabd6f31b45daa0c191f9a97e376c318d0
Paxton Walker <pax@isphere.net>
2022-04-22 16:35:13 -0500

    changed minimize behavior to minimize to system tray

--- WeatherReminder/BaseForm.Designer.cs
+++ WeatherReminder/BaseForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BaseForm));
             this.BaseFormMenuStrip = new System.Windows.Forms.MenuStrip();
             this.emailToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -71,6 +72,7 @@
             this.AddNotificationButton = new System.Windows.Forms.Button();
             this.WelcomeUserLabel = new System.Windows.Forms.Label();
             this.BaseFormSplitLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.trayNotifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
             this.BaseFormMenuStrip.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
@@ -100,6 +102,7 @@
             this.BaseFormMenuStrip.Size = new System.Drawing.Size(397, 24);
             this.BaseFormMenuStrip.TabIndex = 0;
             this.BaseFormMenuStrip.Text = "menuStrip1";
+            this.BaseFormMenuStrip.Resize += new System.EventHandler(this.sendTrayIcon);
             // 
             // emailToolStripMenuItem
             // 
@@ -546,6 +549,13 @@
             this.BaseFormSplitLayout.Size = new System.Drawing.Size(365, 526);
             this.BaseFormSplitLayout.TabIndex = 2;
             // 
+            // trayNotifyIcon
+            // 
+            this.trayNotifyIcon.BalloonTipTitle = "Hello";
+            this.trayNotifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("trayNotifyIcon.Icon")));
+            this.trayNotifyIcon.Text = "Weather Node";
+            this.trayNotifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.trayNotifyIcon_MouseDoubleClick);
+            // 
             // BaseForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -634,5 +644,6 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.NumericUpDown hotNumeric;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NotifyIcon trayNotifyIcon;
     }
 }
